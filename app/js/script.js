687d7384dbafbfb8afde73546c5c563f6e896b5e
AKD47 <van83horn@gmail.com>
2018-03-28 13:43:14 +0300

    css fix

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,31 @@
 $(document).ready(function(){
 
+    /*scroll block*/
+    if(window.innerWidth > 1024) {
+        var element = $('.js-banner');//елемент, который необходимо сделать фиксированным
+        if (element.length > 0) {//проверка наличия элемента на странице
+            var elementPosition = element.offset().top;//определяем позицию элемента относительно верха документа
+            $(window).scroll(function () {//при прокрутке окна браузера
+                fixedScroll(element,
+                    elementPosition, $('.pagination'));//вызывается функция с заданными селекторами
+            });
+        }
+    }
+    /*close scroll block*/
+
 });
+
+function fixedScroll(element, elementPosition, blockElement) {//функция фиксированногоблока, с селекторами элемента, его позиционирования и преграждающего блока
+    var top = $(document).scrollTop(),//значение отступа прокрутки сверху для первого элемента
+        blockingElement = blockElement.offset().top,//позиция блокирующего блока отностительно верха документа
+        height = element.outerHeight();//высота элемента, включающая внутренние и внешние отступы
+    if (top > elementPosition && top < blockingElement - height) {//
+        element.addClass('js-fixed').removeAttr("style");
+    }
+    else if (top > blockingElement - height) {
+        element.removeClass('js-fixed').css({'position': 'absolute', 'bottom': '50px'});
+    }
+    else {
+        element.removeClass('js-fixed');
+    }
+}
